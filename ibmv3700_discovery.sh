e0b5e741db1cf21aef4d5a947b8e0d5fe23c34fe
cuilster <mk.naumburg@gmail.com>
2018-02-09 08:34:13 +0100

    Update ibmv3700_discovery.sh
    #1 - pubkey parameter added for ssh connection
    #2 - function drive; line 32 - cut is changed to dynamically find position of slot_id (changes f.e. if techtype is

--- ibmv3700_discovery.sh
+++ ibmv3700_discovery.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
 SSHCMD=`which ssh`
-REPODIR=/etc/zabbix/scripts/repo
+REPODIR=/usr/lib/zabbix/externalscripts
 STORWIZEADDR=${1}
 
 IFS='
 '
+system(){
+	${SSHCMD} zabbix@${STORWIZEADDR} -i /home/zabbix/.ssh/id_rsa "svcinfo lssystem; svcinfo lssystemstats; svcinfo lsenclosure" > ${REPODIR}/${STORWIZEADDR}.system.repo.tmp && mv ${REPODIR}/${STORWIZEADDR}.system.repo.tmp ${REPODIR}/${STORWIZEADDR}.system.repo
 
-system (){
-        ${SSHCMD} zabbix@${STORWIZEADDR} "svcinfo lssystem; svcinfo lssystemstats; svcinfo lsenclosure" > ${REPODIR}/${STORWIZEADDR}.system.repo.tmp && mv ${REPODIR}/${STORWIZEADDR}.system.repo.tmp ${REPODIR}/${STORWIZEADDR}.system.repo
+	echo "{
+	   \"data\":[
 
-        echo "{
-	       \"data\":[
-
-                {
-                        \"{#STORWIZEADDR}\":\"${STORWIZEADDR}\"}]}"
+		{
+		       \"{#STORWIZEADDR}\":\"${STORWIZEADDR}\"}]}"
 }
 
-drive (){
-        ${SSHCMD} zabbix@${STORWIZEADDR} "svcinfo lsdrive" > ${REPODIR}/${STORWIZEADDR}.drive.repo.tmp && mv ${REPODIR}/${STORWIZEADDR}.drive.repo.tmp ${REPODIR}/${STORWIZEADDR}.drive.repo
-
+drive(){
+        ${SSHCMD} zabbix@${STORWIZEADDR} -i /home/zabbix/.ssh/id_rsa "svcinfo lsdrive" > ${REPODIR}/${STORWIZEADDR}.drive.repo.tmp && mv ${REPODIR}/${STORWIZEADDR}.drive.repo.tmp ${REPODIR}/${STORWIZEADDR}.drive.repo
+		
+	#START CHAR SLOT ID
+        	SCS=$(cat  ${REPODIR}/${STORWIZEADDR}.drive.repo | grep -aob 'lot_id' | \grep -oE '^[0-9]+')
+    	#END CHAR SLOT ID
+        	ECS=$(($SCS + 7))
+	
 	echo "{
 	        \"data\":[
 	                " > ${REPODIR}/${STORWIZEADDR}.drive.tmp
-
 	        for DRIVE in $(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | egrep -v ^id);
 		do
-       	        ID=$(echo ${DRIVE} | cut -c102-108 | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
-	       	        echo "                  	{\"{#SLOTID}\":"\"${ID}"\",\"{#STORWIZEADDR}\":"\"${STORWIZEADDR}"\"}," >> ${REPODIR}/${STORWIZEADDR}.drive.tmp
+       	        ID=$(echo ${DRIVE} | cut -c${SCS}-${ECS} | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+	       	        echo " {\"{#SLOTID}\":"\"${ID}"\",\"{#STORWIZEADDR}\":"\"${STORWIZEADDR}"\"}," >> ${REPODIR}/${STORWIZEADDR}.drive.tmp
 	        done
 		cat ${REPODIR}/${STORWIZEADDR}.drive.tmp | sed '$s/},/}]}/g' && rm -f ${REPODIR}/${STORWIZEADDR}.drive.tmp
 }
 
-volume (){
-	${SSHCMD} zabbix@${STORWIZEADDR} "svcinfo lsvdisk" > ${REPODIR}/${STORWIZEADDR}.volume.repo.tmp &&  mv ${REPODIR}/${STORWIZEADDR}.volume.repo.tmp ${REPODIR}/${STORWIZEADDR}.volume.repo
-
+volume(){
+	${SSHCMD} zabbix@${STORWIZEADDR} -i /home/zabbix/.ssh/id_rsa "svcinfo lsvdisk" > ${REPODIR}/${STORWIZEADDR}.volume.repo.tmp && mv ${REPODIR}/${STORWIZEADDR}.volume.repo.tmp ${REPODIR}/${STORWIZEADDR}.volume.repo
         echo "{
                 \"data\":[
                         " > ${REPODIR}/${STORWIZEADDR}.volume.tmp
-
                 for VOLUME in $(cat ${REPODIR}/${STORWIZEADDR}.volume.repo | egrep -v ^id);
                 do
                 VOLNAME=$(echo ${VOLUME} | awk '{ print $2}')
                         STATUS=$(echo ${VOLUME} | awk '{ print $5}')
-                        echo "                          {\"{#VOLNAME}\":"\"${VOLNAME}"\",\"{#STORWIZEADDR}\":"\"${STORWIZEADDR}"\"}," >> ${REPODIR}/${STORWIZEADDR}.volume.tmp
+                        echo " {\"{#VOLNAME}\":"\"${VOLNAME}"\",\"{#STORWIZEADDR}\":"\"${STORWIZEADDR}"\"}," >> ${REPODIR}/${STORWIZEADDR}.volume.tmp
                 done
                 cat ${REPODIR}/${STORWIZEADDR}.volume.tmp | sed '$s/},/}]}/g' && rm -f ${REPODIR}/${STORWIZEADDR}.volume.tmp
 }
-
 
 if [[ "$2" == "drive" || "$2" == "system" || "$2" == "volume" ]]; then
 	$2
