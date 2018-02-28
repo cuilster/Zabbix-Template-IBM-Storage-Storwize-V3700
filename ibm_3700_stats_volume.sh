973064308cc915fecbc30d14b1495973b2b22fb7
Nikola Belavic <nbelavic@gmail.com>
2018-02-28 11:48:09 +0100

    Update ibm_3700_stats_volume.sh

--- ibm_3700_stats_volume.sh
+++ ibm_3700_stats_volume.sh
@@ -15,16 +15,7 @@ status () {
 
 size () {
         LINE=$(cat ${REPODIR}/${STORWIZEADDR}.volume.repo | awk '{ print $2, $8}' | grep -w ${VOLNAME})
-        VALUE=$(echo ${LINE}| awk '{ print $2 }')
-
-	if [ $(echo ${VALUE} | grep TB) ] ; then
-		VCALC=$(echo ${VALUE} | awk -F'TB' '{ print $1 }')
-		SIZE=$(echo "scale=0; ${VCALC} * 1000" | bc)
-	else
-		VCALC=$(echo ${VALUE} | awk -F'GB' '{ print $1 }')
-		SIZE=$(echo "scale=0; ${VCALC}" | bc)
-	fi
-
+        SIZE=$(echo ${LINE}| awk '{ print $2 }')
 	echo ${SIZE}
 }
 
