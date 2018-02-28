0335e2d9e9ad61ce7b1aeccdf04dbae81181a1ef
Nikola Belavic <nbelavic@gmail.com>
2018-02-28 11:37:23 +0100

    Update ibm_3700_stats_drive.sh

--- ibm_3700_stats_drive.sh
+++ ibm_3700_stats_drive.sh
@@ -14,35 +14,42 @@ ECS=$(($SCS + 7))
 IFS=' '
 
 status () {
-	LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c4-10,${SCS}-${ECS} | grep -w ${SLOTID})
-	STATUS=$(echo ${LINE}| awk '{ print $1 }')
-	echo ${STATUS}
+        LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c4-10,${SCS}-${ECS} --output-delimiter=" " | grep -w ${SLOTID})
+        STATUS=$(echo ${LINE}| awk '{ print $1 }')
+        echo ${STATUS}
 }
 
 use () {
-	LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c33-39,${SCS}-${ECS} | grep -w ${SLOTID})
+        LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c33-39,${SCS}-${ECS} --output-delimiter=" " | grep -w ${SLOTID})
         USE=$(echo ${LINE}| awk '{ print $1 }')
         echo ${USE}
 }
 
 type () {
-	LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c40-49,${SCS}-${ECS} | grep -w ${SLOTID})
+        DSCHR=$(cat  ${REPODIR}/${STORWIZEADDR}.drive.repo | grep -aob 'ech_type' | \grep -oE '^[0-9]+')
+        DECHR=$(($DSCHR + 10))
+        LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c${DSCHR}-${DECHR},${SCS}-${ECS} --output-delimiter=" " | grep -w ${SLOTID})
         TYPE=$(echo ${LINE}| awk '{ print $1 }')
-	echo ${TYPE}
+        echo ${TYPE}
 }
 
 mdisk_name  () {
-        LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c68-78,${SCS}-${ECS} | grep -w ${SLOTID})
-	if [ $(echo ${LINE} | grep -o " " | wc -l) -gt 0  ] ; then
-	        MDISK_NAME=$(echo ${LINE}| awk '{ print $1 }')
-	fi
-	echo ${MDISK_NAME}
+        DSCHR=$(cat  ${REPODIR}/${STORWIZEADDR}.drive.repo | grep -aob 'disk_name' | \grep -oE '^[0-9]+')
+        DECHR=$(($DSCHR + 10))
+        LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c${DSCHR}-${DECHR},${SCS}-${ECS} --output-delimiter=" " | grep -w ${SLOTID})
+        if [ $(echo ${LINE} | grep -o " " | wc -l) -gt 0  ] ; then
+                MDISK_NAME=$(echo ${LINE}| awk '{ print $1 }')
+        fi
+        echo ${MDISK_NAME}
 }
 
 enclosure_id () {
-	LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c89-101,${SCS}-${ECS} | awk -v SLOTID=${SLOTID} '{if ($2==SLOTID){print $0}}')
-	ENCLOUSURE_ID=$(echo ${LINE}| awk '{ print $1 }')
-	echo ${ENCLOUSURE_ID}
+        DSCHR=$(cat  ${REPODIR}/${STORWIZEADDR}.drive.repo | grep -aob 'nclosure_id' | \grep -oE '^[0-9]+')
+        DECHR=$(($DSCHR + 4))
+        LINE=$(cat ${REPODIR}/${STORWIZEADDR}.drive.repo | cut -c${DSCHR}-${DECHR},${SCS}-${ECS} --output-delimiter=" " | awk -v SLOTID=${SLOTID} '{if ($2==SLOTID){print $0}}')
+        ENCLOUSURE_ID=$(echo ${LINE}| awk '{ print $1 }')
+        echo ${ENCLOUSURE_ID}
 }
 
 $3
+
